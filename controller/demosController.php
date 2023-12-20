ad8cee58abb5c711d7aeb5fcdedfa29b667972c0
Dragan Constantin <constantin.dragan@efrei.net>
2023-12-20 22:01:26 +0100

    Fixed SoundCloud iframe display

--- controller/demosController.php
+++ controller/demosController.php
@@ -12,16 +12,13 @@ class DemosController {
     public function getDemosPage() {
         include_once('view/demos.php');
         $data=$this->model->getMusics();
-        // var_dump($data);
         $links=[];
         for($i=0;$i<count($data);$i++){
-            // var_dump($data[$i]);
             $links[]=$data[$i]["link"];
         }
-        // var_dump($links);
         foreach($links as $link){
+            $source = "src='$link'";
             include("view/music.php");
-            // $source="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/$link&color=%23eab308&auto_play=false&hide_related=false&show_comments=false&show_user=false&show_reposts=false&show_teaser=true";
         }
     }
 }
--- view/music.php
+++ view/music.php
@@ -1,5 +1,5 @@
 <div class="columns-demos">
     <ul class="stat">
-        <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/<? echo $link ?>&color=%23eab308&auto_play=false&hide_related=false&show_comments=false&show_user=false&show_reposts=false&show_teaser=true"></iframe>
+        <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="<?php echo $link?>"></iframe>
     </ul>
 </div>
