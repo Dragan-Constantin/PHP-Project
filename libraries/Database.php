a4884dd32cf918fb981e4db86ea54ba05d2db602
Dragan Constantin <constantin.dragan@efrei.net>
2023-12-20 22:00:23 +0100

    Fixed connection to db

--- libraries/Database.php
+++ libraries/Database.php
@@ -1,23 +1,23 @@
 <?php
 
 class Database {
-    private $host = "localhost";
-    private $username = "root";
-    private $password = "";
-    private $database = "php_course_pdo";
+    private $host = "51.158.59.186";
+    private $username = "phppex";
+    private $password = "Supermotdepasse!42";
+    private $database = "'CD'";
 
     private $conn;
     private $stmt;
     private $error;
     
     public function __construct() {
-        $dsn = "mysql:host=$this->host;dbname=$this->database";
+        $dsn = "mysql:host=$this->host;port=14301;dbname=$this->database";
         $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_PERSISTENT => true);
         try {
             $this->conn = new PDO($dsn, $this->username, $this->password, $options);
         }
         catch (PDOException $e) {
-            $this->error_code = $e->getCode();
+            // $this->error_code = $e->getCode();
             $this->error = $e->getMessage();
         }
     }
