5654e855b1b36441d0522b8e95182efab3972729
Dragan Constantin <constantin.dragan@efrei.net>
2023-12-20 22:01:42 +0100

    Added footer and delete unnecessary comments

--- router.php
+++ router.php
@@ -6,19 +6,16 @@ function route_request($address){
         echo '<meta http-equiv="refresh" content="0;url=home">';
     }
     elseif ($address === "/home"){
-        // include_once("view/home.php");
         include_once("controller/homeController.php");
         $page = new HomeController;
         $page->getHomePage();
     }
     elseif ($address === "/about"){
-        // include_once("view/about.php");
         include_once("controller/aboutController.php");
         $page = new AboutController;
         $page->getAboutPage();
     }
     elseif ($address === "/demos"){
-        // include_once("view/music-demos.php");
         include_once("controller/demosController.php");
         $page = new DemosController;
         $page->getDemosPage();
@@ -26,5 +23,7 @@ function route_request($address){
     else{
         include_once("view/404.php");
     }
+    $year = date("Y");
+    $name = "Dragan Constantin";
     include_once("view/footer.php");
 }
