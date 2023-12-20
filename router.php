b6deb6335288b61748c1081713c2d7c093107513
Dragan Constantin <constantin.dragan@efrei.net>
2023-12-21 00:05:26 +0100

    Changed ifelse into switch case

--- router.php
+++ router.php
@@ -2,27 +2,30 @@
 
 function route_request($address){
     include_once("view/header.php");
-    if ($address === "/") {
-        echo '<meta http-equiv="refresh" content="0;url=home">';
+    switch ($address) {
+        case "/":
+            echo '<meta http-equiv="refresh" content="0;url=home">';
+            break;
+        case "/home":
+            include_once("controller/homeController.php");
+            $page = new HomeController;
+            $page->getHomePage();
+            break;
+        case "/about":
+            include_once("controller/aboutController.php");
+            $page = new AboutController;
+            $page->getAboutPage();
+            break;
+        case "/demos":
+            include_once("controller/demosController.php");
+            $page = new DemosController;
+            $page->getDemosPage();
+            break;
+        default:
+            include_once("view/404.php");
+            break;
     }
-    elseif ($address === "/home"){
-        include_once("controller/homeController.php");
-        $page = new HomeController;
-        $page->getHomePage();
-    }
-    elseif ($address === "/about"){
-        include_once("controller/aboutController.php");
-        $page = new AboutController;
-        $page->getAboutPage();
-    }
-    elseif ($address === "/demos"){
-        include_once("controller/demosController.php");
-        $page = new DemosController;
-        $page->getDemosPage();
-    }
-    else{
-        include_once("view/404.php");
-    }
+    
     $year = date("Y");
     $name = "Dragan Constantin";
     include_once("view/footer.php");
